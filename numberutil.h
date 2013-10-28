4324c8abc725a19d35e8762fe1c8c62b95639b44
Aleksi Pihkanen <alx@iki.fi>
2013-10-28 15:32:45 +0200

    working around problem with sscanf usage

--- numberutil.h
+++ numberutil.h
@@ -3,11 +3,17 @@ int32_t dmmtoint(char *str, char *hemi)
 {
 	// input format is DDDMM.MMMM
 	int degrees;
-    float minutes;
+	float minutes;
 	int32_t r;
-	if (str[5] == '.') sscanf(str,"%3d%f",&degrees,&minutes);
-	else sscanf(str,"%2d%f",&degrees,&minutes);
-    r = degrees * 60000 + minutes * 1000;
-	if (hemi && (*hemi == 'S' || *hemi == 'W')) return(-1*r);
+	if (str[5] == '.') {
+		sscanf(str, "%3d%f", &degrees, &minutes);	// *FIXME* scanning minutes does not work
+		minutes = atof(str + 3);
+	} else {
+		sscanf(str, "%2d%f", &degrees, &minutes);	// *FIXME* scanning minutes does not work
+		minutes = atof(str + 2);
+	}
+	r = (60 * degrees + minutes) * 1000;
+	if (hemi && (*hemi == 'S' || *hemi == 'W'))
+		return (-1 * r);
 	return (r);
 }
